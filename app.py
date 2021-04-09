2f85d457682de88a116e26cf8e0f727ac30dc39c
Sreeram S <sreeramjunior@gmail.com>
2021-04-09 16:09:58 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,18 @@
+import tensorflow as tf
+import numpy as np
+import gradio as gr
+
+from urllib.request import urlretrieve
+
+model = tf.keras.models.load_model("mnist-model.h5")
+
+def recognize_digit(image):
+    image = image.reshape(1, -1)  # add a batch dimension
+    prediction = model.predict(image).tolist()[0]
+    return {str(i): prediction[i] for i in range(10)}
+
+output_component = gr.outputs.Label(num_top_classes=3)
+
+gr.Interface(fn=recognize_digit, 
+             inputs="sketchpad", 
+             outputs=output_component).launch()
